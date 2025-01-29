78ac309454648b4c2ede01225509a6e05758ff23
Justin Ho <justinho@Justins-MacBook-Air.local>
2025-01-28 21:11:03 -0800

    new projects section

--- src/components/mod-programs/projects/ModProjects.js
+++ src/components/mod-programs/projects/ModProjects.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Nav, Image } from "react-bootstrap";
+import "./ModProjects.css";
+//This is the card card of Project Workshops
 
-//This is the card card of Project Workshops 
 const ModProjects = () => {
 
     return (
     <>
         <Card>
-            <Card.Title className="text-center">Projects</Card.Title>
+            <Card.Title className="text-center title">Projects</Card.Title>
             <Card.Body>
-                <div>You Divs or anything can go here</div>
+                <div>Join our semester-long workshop to enhance your coding skills through a hands-on project and expert guidance from our ACM leaders. 
+                    Perfect for students eager to advance their practical programming abilities while adding to their resume.
+                    
+                </div>
+                
+                <div className = "figureContainer">
+                    {/*placeholder for images of past project posters*/}
+                    <Image src = {require("./photos/game_of_life_poster.png")} rounded/>
+                    <Image src = {require("./photos/space_invaders_poster.png")} rounded/>                    
+                    <Image src = {require("./photos/wordle_poster.png")} rounded/>
+                    
+                    
+                </div>
+
+                {/*Not sure what I'm supposed to link to  */}
+                <div className = "link">
+                    <Nav.Link href = "./projects/CurrentProjects"><Image src = {require("./photos/link.png")}></Image>See what we're doing this semester!</Nav.Link>
+                </div>
+                
+
+
             </Card.Body>
 
         </Card>
